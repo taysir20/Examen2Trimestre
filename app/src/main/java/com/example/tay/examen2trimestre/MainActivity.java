989968b0d5216e388a2ae415423a533a24b0266d
Taysir Al-Shareef Pinero <taysirasp@gmail.com>
2018-02-19 09:36:09 +0100

    Añadido callBackManager, loginButton de facebook en el MainActivity

--- app/src/main/java/com/example/tay/examen2trimestre/MainActivity.java
+++ app/src/main/java/com/example/tay/examen2trimestre/MainActivity.java
@@ -3,11 +3,57 @@ package com.example.tay.examen2trimestre;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Definimos un CallBackManager que se encargará de realizar y devolver llamadas a facebook
+    CallbackManager callbackManager;
+    //Definimos el botón de logueo de facebook para asociarlo a su componente visual
+    LoginButton loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Crea un administrador de devoluciones de llamada que gestione las respuestas de inicio de sesión.
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        //Añadimos los permisos que queramos solicitar al usuario en el momento en el que se loguea
+        loginButton.setReadPermissions(Arrays.asList
+                ("public_profile", "user_friends", "email", "user_photos", "  user_birthday", "read_custom_friendlists"));
+
+        /*
+           A través del registercallbaxk registraremos las llamadas a facebook y si si son exitosas o dan error se llamará a uno de estos
+           métodos
+         */
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
     }
 }
